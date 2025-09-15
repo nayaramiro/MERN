2ff8a8b348c3cfc510204da928f78e06348a2c65
nayaramiro <nayaramiro.be@gmail.com>
2025-09-15 22:01:59 +0200

    bcryptjs

--- cours_01/src/models/User.js
+++ cours_01/src/models/User.js
@@ -1,6 +1,6 @@
 //nouveau model user pour la base de donnée
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 // création du schéma user
 //Le schema sert à définir la structure des documents dans une collection MongoDB
 const userSchema = new mongoose.Schema({
@@ -20,6 +20,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    //pour ne pas montrer le pwd dans la db
+    select: false,
   },
   avatar: {
     type: String,
@@ -29,6 +31,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+//avant de sauvegarder le schema, fais le pre
+userSchema.pre("save", async function (next) {
+  //dans userSchema va dans password et créé un hash = un random cripto de mon password
+  //bcrypt reçoit deux paramètres : ce qu'il doit cripter et combien de tour il va faire
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 //creéation d'un nouveau model qui s'appelle User, avec le schéma userSchema
